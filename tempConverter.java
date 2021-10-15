0f7c4a756205b7d66db7847d8127b5bfd1f585d0
APotatoChip <cvetina.a.m@gmail.com>
2021-10-15 14:41:14 +0300

    second exercise

---
+++ tempConverter.java
@@ -0,0 +1,1 @@
+public class TempConverter{public static void main(String[] args) {    double farenheit = 98.0;    double celsius=5*(farenheit-32)/9;    System.out.println("Temperature in Fahrenheit is: "+farenheit);    System.out.println("Temperature in Celsius is: "+celsius);       }} 
