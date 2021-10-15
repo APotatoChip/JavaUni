c655779b2ff18a1fd5f48a086adc90a4008c37b4
APotatoChip <cvetina.a.m@gmail.com>
2021-10-15 14:46:08 +0300

    same

--- tempConverter.java
+++ tempConverter.java
@@ -1,1 +1,12 @@
-public class TempConverter{public static void main(String[] args) {    double farenheit = 98.0;    double celsius=5*(farenheit-32)/9;    System.out.println("Temperature in Fahrenheit is: "+farenheit);    System.out.println("Temperature in Celsius is: "+celsius);       }} 
+public class TempConverter
+{
+	public static void main(String[] args) {
+	    double farenheit = 98.0;
+	    double celsius=5*(farenheit-32)/9;
+	    System.out.println("Temperature in Fahrenheit is: "+farenheit);
+	    System.out.println("Temperature in Celsius is: "+celsius);
+	    
+	   
+	}
+
+}
--- triangleArea.java
+++ triangleArea.java
@@ -1,1 +1,15 @@
-import java.util.Scanner;public class TriangleArea{public static void main(String[] args) {   Scanner scan = new Scanner(System.in); System.out.println("Please enter A: "); double a = scan.nextDouble(); System.out.println("Please enter C (hypotenuse): "); double c = scan.nextDouble(); double b = Math.sqrt((c*c)- (a*a)); double area = (a * b) / 2; System.out.printf("The area of the triangle is " + "%.3f",area); scan.close();}} 
+import java.util.Scanner;
+public class TriangleArea
+{
+	public static void main(String[] args) {
+	   Scanner scan = new Scanner(System.in);
+ System.out.println("Please enter A: ");
+ double a = scan.nextDouble();
+ System.out.println("Please enter C (hypotenuse): ");
+ double c = scan.nextDouble();
+ double b = Math.sqrt((c*c)- (a*a));
+ double area = (a * b) / 2;
+ System.out.printf("The area of the triangle is " + "%.3f",area);
+ scan.close();
+	}
+}
